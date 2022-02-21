3db8f4b90134051aefcac80bc1274c53b4070399
spirzad <syed.taha.pirzada@intel.com>
2022-02-20 19:25:54 -0700

    Update work.py

--- work.py
+++ work.py
@@ -1,23 +1,16 @@
 import sys
-sys.path.insert(0, "..\\modules")
+sys.path.append('./modules')
 
 
 if __name__ == "__main__":
     import json
     import glob
     import os.path
-    from Onboarding_Checklist_Module import Onboarding_Checklist
-    from flask_restful import reqparse
-    
-    # Parsing argument in the API.
-    parser = reqparse.RequestParser()   #/isdm_onboarding?type=yaml
-    parser.add_argument("type", type=str)
-    args = parser.parse_args() 
-        
+    from Onboarding_Checklist_Module import Onboarding_Checklist        
         
     checklist = Onboarding_Checklist(auth_token="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")
-    type = args["type"]
-        
+    type = "yaml"
+    
     if type == "yaml":
         checklist.check_for_yaml()
         data = checklist.get_maintainers()
