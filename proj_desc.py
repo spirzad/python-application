490b9f950f940f06da79400f876d1e6d023a402d
spirzad <syed.taha.pirzada@intel.com>
2022-02-22 16:53:40 -0700

    Update proj_desc.py

--- proj_desc.py
+++ proj_desc.py
@@ -42,6 +42,8 @@ print("INFO - PR_Desc", pr_desc)
 if "AUTHOR" in pr_desc:
     response = go.post_comment_by_pr(org=repo_org, repo_name=repo_name, pull_number=pr_number, comment="Author is present in PR Description.")
     review_bot = go.post_bot_review(org=repo_org, repo_name=repo_name, pull_number=pr_number, comment="Approving changes.", event="APPROVE")
+    print("INFO - ", review_bot)
 else:
     response = go.post_comment_by_pr(org=repo_org, repo_name=repo_name, pull_number=pr_number, comment="This is crazy comment.")
     review_bot = go.post_bot_review(org=repo_org, repo_name=repo_name, pull_number=pr_number, comment="Requesting changes.", event="REQUEST_CHANGES")
+    print("INFO - ", review_bot)
