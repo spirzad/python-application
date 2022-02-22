eb5e4ff80061861dae319074d6b6b1a49db80c96
spirzad <syed.taha.pirzada@intel.com>
2022-02-22 14:39:05 -0700

    Update proj_desc.py
    Adding post comment method.

--- proj_desc.py
+++ proj_desc.py
@@ -6,24 +6,39 @@ import argparse
 if __name__ == "__main__":
     
   parser = argparse.ArgumentParser()
-  parser.add_argument("--auth_token", "-a", help="You need to provide auth token.",type=str, required=True)
+  parser.add_argument("--auth_token", "-a", help="You need to provide auth token",type=str, required=True)
+  parser.add_argument("--pull_number", "-pr", help="You need to provide pull_number",type=str, required=True)
+  parser.add_argument("--repo_org", "-ro", help="You need to provide repo org",type=str, required=True)
+  parser.add_argument("--repo_name", "-rn", help="You need to provide repo name",type=str, required=True)
   args = parser.parse_args()
   
   # Getting auth token.
   auth_token = str(args.auth_token)
+  pr_number = str(args.pull_number)
+  repo_org = str(args.repo_org)
+  repo_name = str(args.repo_name)
   
   # Github Class Instantiation.
   go = GitHubClass(auth_token=auth_token)
   go.generate_header()
   
-  repo_names = ["python-application"]
-  
+  repo_names = list(repo_name)
+  pr_desc = ""
   for repo in repo_names:
-      repo_data = go.get_pulls_allstate_by_repo("spirzad" ,repo, pull_type="per_page")
+      repo_data = go.get_pulls_allstate_by_repo(repo_org ,repo, pull_type="per_page")
       
       for data in repo_data:
           pull_number = data["number"]
           state = data["state"]
           message = data["body"]
           
-          print("PR Description : ", message)
+          if int(pull_number) == int(pr_number):
+                pr_desc = message
+                break
+print("INFO - PR : ", pr_number)
+print("INFO - PR_Desc", pr_desc)
+
+if("AUTHOR") in pr_desc:
+    response = go.post_comment_by_pr(org=repo_org, repo_name=repo_name, pull_number=pull_number, "Author is present in PR Description.")
+else:
+    response = go.post_comment_by_pr(org=repo_org, repo_name=repo_name, pull_number=pull_number, "This is crazy comment.")
